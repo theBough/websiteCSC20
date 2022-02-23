c06e0af5fcc10703679632bd91328a0e0cac3022
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-23 12:49:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var ans = document.getElementById("answer")
+function myFunction(){
+  //This function will tell,
+  //our website visitor that they are 
+  //the best
+  ans.innerHTML = "You're the best!!"
+}
